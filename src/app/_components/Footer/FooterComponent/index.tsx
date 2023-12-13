cec100bbdd1da00fa4e19273aba2824e021b6d6c
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-12-13 01:37:55 -0500

    small changes

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -21,25 +21,6 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
 
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
-      <Gutter>
-        <ul className={classes.inclusions}>
-          {inclusions.map(inclusion => (
-            <li key={inclusion.title}>
-              <Image
-                src={inclusion.icon}
-                alt={inclusion.title}
-                width={36}
-                height={36}
-                className={classes.icon}
-              />
-
-              <h5 className={classes.title}>{inclusion.title}</h5>
-              <p>{inclusion.description}</p>
-            </li>
-          ))}
-        </ul>
-      </Gutter>
-
       <div className={classes.footer}>
         <Gutter>
           <div className={classes.wrap}>
